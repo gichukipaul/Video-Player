cbedc8b1c0bdea144d24cef3f6bfc77fc1b9fa35
gichukipaul <gichukipaul043@gmail.com>
2021-03-22 20:50:46 +0300

    Update : persisting playbackstate

--- app/src/main/java/io/alienlabs/gichukipaul/vdeo/Exoplayer.java
+++ app/src/main/java/io/alienlabs/gichukipaul/vdeo/Exoplayer.java
@@ -4,7 +4,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.exoplayer2.MediaItem;
@@ -13,9 +12,7 @@ import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.ui.PlayerView;
 
 public class Exoplayer extends AppCompatActivity {
-    private PlayerView playerView;
     private static Player player;
-    private long progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,52 +20,53 @@ public class Exoplayer extends AppCompatActivity {
         setContentView(R.layout.activity_exoplayer);
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        playerView = findViewById(R.id.video);
-        progress = savedInstanceState != null ? savedInstanceState.getLong("progress") : 0;
+        PlayerView playerView = findViewById(R.id.video);
         player = new SimpleExoPlayer.Builder(this).build();
         playerView.setPlayer(player);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             Uri uri = Uri.parse(bundle.getString("uri"));
             MediaItem mediaItem = MediaItem.fromUri(uri);
-            progress = player.getContentPosition();
             player.setMediaItem(mediaItem);
+            player.prepare();
         }
+
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onPlaybackStateChanged(int state) {
+
+            }
+
+            @Override
+            public void onPlayWhenReadyChanged(boolean playWhenReady, int reason) {
+
+            }
+
+            @Override
+            public void onRepeatModeChanged(int repeatMode) {
+
+            }
+        });
     }
 
     @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putLong("progress", progress);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        player.seekTo(progress);
-        player.prepare();
+    protected void onRestart() {
+        super.onRestart();
+        player.setPlayWhenReady(true);
+        player.getPlaybackState();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        player.seekTo(0,progress);
         player.setPlayWhenReady(true);
-        player.play();
+        player.getPlaybackState();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        progress = player.getContentPosition();
-        player.pause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        player.stop();
-        progress = player.getContentPosition();
-
+        player.setPlayWhenReady(false);
+        player.getPlaybackState();
     }
 }
